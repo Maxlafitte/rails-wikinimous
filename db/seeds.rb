bd8b73810bf69e4c8e8aa07c16481e2d6b8e8cc6
Maxime Lafitte <maxime.lafitte@gmail.com>
2019-02-12 17:24:11 +0100

    rails wiki done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+10.times do
+  article = Article.new(
+    title: Faker::Artist.name,
+    content: Faker::Quote.yoda,
+  )
+  article.save
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
